41540238a535f9c1cc15862fcd330af2db4772b0
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-01 23:36:08 -0300

    First commit aula 01.03.2021
    Aula 01.03.2021

---
+++ app/src/main/java/com/douglasvilella/meuprimeiroprojeto/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.douglasvilella.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    fun jogarDado(view: View){
+        var texto = findViewById<View>(R.id.txtMensagem) as TextView
+        texto.setText("Teste")
+    }
+}
