788cc61191c77e34f6d52b03fb3618f2295d6673
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-03 22:25:09 -0300

    commit app slash activity

---
+++ app/src/main/java/com/douglasvilella/meuprimeiroprojeto/SlashActivity.kt
@@ -0,0 +1,11 @@
+package com.douglasvilella.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SlashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slash)
+    }
+}
