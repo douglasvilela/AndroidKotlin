bf3c5bfb16faa83949004391adefaf4fa9576141
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-03 22:48:51 -0300

    Main + Splash Activity OK

--- app/src/main/java/com/douglasvilella/meuprimeiroprojeto/SplashActivity.kt
+++ app/src/main/java/com/douglasvilella/meuprimeiroprojeto/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.douglasvilella.meuprimeiroprojeto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slash)
+        // Executando instruções após determinado tempo
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Uma das utilidades da classe Intent é usá-la para abrir uma activity
+            val mIntent = Intent(this@SplashActivity,MainActivity::class.java)
+
+            //Método responsável por executar a Intent
+
+            startActivity(mIntent)
+
+            //Remover a tela da pilha
+            finish()
+        }, 5000) //5 segundos - tempo em milisegundos
     }
 }
