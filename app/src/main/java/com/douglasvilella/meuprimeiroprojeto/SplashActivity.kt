7b3792dfc29c5765ffb8504be9be6f6b20ddec6d
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-03 22:26:46 -0300

    Mudei o nome para Splash

--- app/src/main/java/com/douglasvilella/meuprimeiroprojeto/SplashActivity.kt
+++ app/src/main/java/com/douglasvilella/meuprimeiroprojeto/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.douglasvilella.meuprimeiroprojeto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SlashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slash)
